9ce13890b344cf63b97148e1da5e6a08ce536a15
Sávio Mendes <saviomd@gmail.com>
2018-07-18 14:55:42 -0300

    Update exercise 3

--- src/exercises/03.js
+++ src/exercises/03.js
@@ -3,44 +3,29 @@
 import React from 'react'
 import {Switch} from '../switch'
 
-// Right now our component can only clone and pass props to immediate children.
-// So we need some way for our compound components to implicitly accept the on
-// state and toggle method regardless of where they're rendered within the
-// Toggle component's "posterity" :)
-//
-// The way we do this is through context. React.createContext is the API we
-// want. Here's a simple example of that API:
-//
-// const defaultValue = 'light'
-// const ThemeContext = React.createContext(defaultValue)
-//
-// ...
-// <ThemeContext.Provider value={this.state}>
-//   {this.props.children}
-// </ThemeContext.Provider>
-// ...
-//
-// ...
-// <ThemeContext.Consumer>
-//   {value => <div>The current theme is: {value}</div>}
-// </ThemeContext.Consumer>
-// ...
-
-// 🐨 create a ToggleContext with React.createContext here
+const ToggleContext = React.createContext()
 
 class Toggle extends React.Component {
-  // 🐨 each of these compound components will need to be changed to use
-  // ToggleContext.Consumer and rather than getting `on` and `toggle`
-  // from props, it'll get it from the ToggleContext.Consumer value.
-  static On = ({on, children}) => (on ? children : null)
-  static Off = ({on, children}) => (on ? null : children)
-  static Button = ({on, toggle, ...props}) => (
-    <Switch on={on} onClick={toggle} {...props} />
+  static On = ({ children }) => (
+    <ToggleContext.Consumer>
+      { contextValue => (contextValue.on ? children : null) }
+    </ToggleContext.Consumer>
   )
-  // Because we'll be passing state into context, we need to 🐨 add the
-  // toggle function to state.
-  // 💰 You'll need to move this below the `toggle` function. See
-  // if you can figure out why :)
+  static Off = ({ children }) => (
+    <ToggleContext.Consumer>
+      { contextValue => (contextValue.on ? null : children) }
+    </ToggleContext.Consumer>
+  )
+  static Button = props => (
+    <ToggleContext.Consumer>
+      { contextValue => (
+        <Switch
+          on={contextValue.on}
+          onClick={contextValue.toggle}
+          {...props} />
+      ) }
+    </ToggleContext.Consumer>
+  )
   state = {on: false}
   toggle = () =>
     this.setState(
@@ -48,23 +33,13 @@ class Toggle extends React.Component {
       () => this.props.onToggle(this.state.on),
     )
   render() {
-    // Because this.props.children is _immediate_ children only, we need
-    // to 🐨 remove this map function and render our context provider with
-    // this.props.children as the children of the provider. Then we'll
-    // expose the on state and toggle method as properties in the context
-    // value (the value prop).
-    return React.Children.map(this.props.children, child =>
-      React.cloneElement(child, {
-        on: this.state.on,
-        toggle: this.toggle,
-      }),
-    )
+    return <ToggleContext.Provider value={ {
+      on: this.state.on,
+      toggle: this.toggle,
+    } }>{ this.props.children }</ToggleContext.Provider>
   }
 }
 
-// Don't make changes to the Usage component. It's here to show you how your
-// component is intended to be used and is used in the tests.
-// You can make all the tests pass by updating the Toggle component.
 function Usage({
   onToggle = (...args) => console.log('onToggle', ...args),
 }) {
