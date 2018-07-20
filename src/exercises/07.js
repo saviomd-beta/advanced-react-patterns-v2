ff543098f492635d4b396f3d9b8217ccee9405a8
Sávio Mendes <saviomd@gmail.com>
2018-07-20 15:04:55 -0300

    Update exercise 7

--- src/exercises/07.js
+++ src/exercises/07.js
@@ -7,21 +7,19 @@ const callAll = (...fns) => (...args) =>
   fns.forEach(fn => fn && fn(...args))
 
 class Toggle extends React.Component {
-  // 🐨 We're going to need some static defaultProps here to allow
-  // people to pass a `initialOn` prop.
-  //
-  // 🐨 Rather than initializing state to have on as false,
-  // set on to this.props.initialOn
-  state = {on: false}
+  static defaultProps = {
+    initialOn: false,
+    onReset: () => {},
+  }
+  initialState = {on: this.props.initialOn}
+  state = this.initialState
 
-  // 🐨 now let's add a reset method here that resets the state
-  // to the initial state. Then add a callback that calls
-  // this.props.onReset with the `on` state.
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+  reset = () => this.setState(this.initialState, () => this.props.onReset(this.state.on))
   getTogglerProps = ({onClick, ...props} = {}) => {
     return {
       'aria-expanded': this.state.on,
@@ -33,8 +31,7 @@ class Toggle extends React.Component {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      // 🐨 now let's include the reset method here
-      // so folks can use that in their implementation.
+      reset: this.reset,
       getTogglerProps: this.getTogglerProps,
     }
   }
@@ -43,9 +40,6 @@ class Toggle extends React.Component {
   }
 }
 
-// Don't make changes to the Usage component. It's here to show you how your
-// component is intended to be used and is used in the tests.
-// You can make all the tests pass by updating the Toggle component.
 function Usage({
   initialOn = false,
   onToggle = (...args) => console.log('onToggle', ...args),
